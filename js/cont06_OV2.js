9fd119a2e23d4a0cb6b1686649a49d102f5f1cb8
krapu1 <krapu1@hotmail.com>
2017-09-14 10:21:23 -0500

    Desarrollo de los contenidos 4,5,6 y 7.

--- js/cont06_OV2.js
+++ js/cont06_OV2.js
@@ -1,4 +1,17 @@
 $("document").ready(function(){
     
     $("#acc_btn_1").trigger("click")
-});
+
+
+
+    /* initiate pugin assigning the desired button labels  */
+    $("div.holder").jPages({
+        containerID : "itemContainer",
+        perPage     : 1,
+        first       : false,
+        previous    : "span.arrowPrev",
+        next        : "span.arrowNext",
+        last        : false
+    });
+
+});  
